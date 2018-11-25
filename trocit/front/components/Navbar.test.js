431f3506737a2cc05bef55d0128013609825a3ed
Alexis Couronne <alexis@skitoo.net>
2018-11-25 22:26:00 +0100

    Add Navbar component

---
+++ trocit/front/components/Navbar.test.js
@@ -0,0 +1,40 @@
+import { mount, createLocalVue } from '@vue/test-utils';
+import Navbar from './Navbar';
+import Burger from './Burger';
+import NavbarMenu from './NavbarMenu';
+
+
+describe('components::Navbar', () => {
+  const vue = createLocalVue();
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(Navbar, { vue });
+  });
+
+  it('should render Navbar', () => {
+    expect(wrapper.classes()).toContain('navbar');
+    expect(wrapper.contains(Burger)).toBeTruthy();
+    expect(wrapper.contains(NavbarMenu)).toBeTruthy();
+  });
+
+  it('should be active menu', () => {
+    expect(wrapper.vm.isActive).toBeFalsy();
+
+    const menu = wrapper.find(NavbarMenu);
+    const burger = wrapper.find(Burger);
+    expect(menu.classes()).not.toContain('is-active');
+    expect(burger.classes()).not.toContain('is-active');
+    wrapper.vm.isActive = true;
+    expect(menu.classes()).toContain('is-active');
+    expect(burger.classes()).toContain('is-active');
+  });
+
+  it('should toggle isActive when user click on Burger button', () => {
+    const burger = wrapper.find(Burger);
+    burger.trigger('click');
+    expect(wrapper.vm.isActive).toBeTruthy();
+    burger.trigger('click');
+    expect(wrapper.vm.isActive).toBeFalsy();
+  });
+});
