16ec06b15826682aa7191cd4c7a48827d9b79e81
Alexis Couronne <alexis@skitoo.net>
2018-11-25 13:12:55 +0100

    Add webpack config file

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, '../trocit/front/index.js'),
+  output: {
+    path: path.resolve(__dirname, '../trocit/static/js'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue/,
+        use: 'vue-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+};
