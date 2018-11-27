0b0a5020ff6f9d58384ef24685fe6b9ab36709b7
Alexis Couronne <alexis@skitoo.net>
2018-11-27 08:07:49 +0100

    Setup vue-i18n

--- trocit/front/index.js
+++ trocit/front/index.js
@@ -1,8 +1,20 @@
 import Vue from 'vue';
+import VueI18n from 'vue-i18n';
 import App from './App';
+import messages from './locales';
+
+Vue.use(VueI18n);
+
+
+const i18n = new VueI18n({
+  locale: 'fr',
+  messages,
+});
+
 
 /* eslint-disable-next-line no-new */
 new Vue({
   el: '#app',
+  i18n,
   render: h => h(App),
 });
---
+++ trocit/front/locales/en.js
@@ -0,0 +1,4 @@
+export default {
+  'Sign up': 'Sign up',
+  'Log in': 'Log in',
+};
