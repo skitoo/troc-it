8b35ab9b3e34f5dc1580f21c451dc3a7ac3accfd
Alexis Couronne <alexis@skitoo.net>
2018-11-30 18:37:26 +0100

    chore: configure vue-awesome library
    - add user icon
    - add lock icon

--- trocit/front/index.js
+++ trocit/front/index.js
@@ -1,11 +1,14 @@
 import Vue from 'vue';
 import VueI18n from 'vue-i18n';
+import Icon from 'vue-awesome/components/Icon';
 import App from './App';
 import messages from './locales';
+import './fontawesome';
 
 require('./styles/style.scss');
 
 Vue.use(VueI18n);
+Vue.component('icon', Icon);
 
 const config = JSON.parse(document.getElementById('config').textContent);
 
