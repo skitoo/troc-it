4b82f1347f2347086e70fbab7dbf5b55d3c16703
Alexis Couronne <alexis@skitoo.net>
2018-12-02 21:01:56 +0100

    chore: initialize Vuex store

--- trocit/front/index.js
+++ trocit/front/index.js
@@ -1,16 +1,20 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
 import VueI18n from 'vue-i18n';
 import Icon from 'vue-awesome/components/Icon';
 import App from './App';
 import messages from './locales';
+import StoreData from './store';
 import './fontawesome';
 
 require('./styles/style.scss');
 
-Vue.use(VueI18n);
-Vue.component('icon', Icon);
-
 const config = JSON.parse(document.getElementById('config').textContent);
+
+Vue.use(VueI18n);
+Vue.use(Vuex);
+
+Vue.component('icon', Icon);
 
 
 const i18n = new VueI18n({
@@ -19,10 +23,13 @@ const i18n = new VueI18n({
   messages,
 });
 
+const store = new Vuex.Store(StoreData);
+
 
 /* eslint-disable-next-line no-new */
 new Vue({
   el: '#app',
   i18n,
+  store,
   render: h => h(App),
 });
